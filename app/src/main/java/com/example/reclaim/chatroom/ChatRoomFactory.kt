d48d8442e40cd65e29b2138f200b187c0e3fe938
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-10-19 21:03:12 +0800

    set repository as parameter in chat room factory

--- app/src/main/java/com/example/reclaim/chatroom/ChatRoomFactory.kt
+++ app/src/main/java/com/example/reclaim/chatroom/ChatRoomFactory.kt
@@ -3,13 +3,14 @@ package com.example.reclaim.chatroom
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.reclaim.data.ReclaimDatabaseDao
+import com.example.reclaim.data.source.ChairRepository
 import java.lang.IllegalArgumentException
 
-class ChatRoomFactory(private val args: ChatRoomFragmentArgs, private val reclaimDatabaseDao: ReclaimDatabaseDao): ViewModelProvider.Factory {
+class ChatRoomFactory(private val chairRepository: ChairRepository, private val args: ChatRoomFragmentArgs): ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ChatRoomViewModel::class.java)) {
-            return ChatRoomViewModel(reclaimDatabaseDao, args) as T
+            return ChatRoomViewModel(chairRepository, args) as T
         }
 
         throw IllegalArgumentException("Unknown viewModel class")
