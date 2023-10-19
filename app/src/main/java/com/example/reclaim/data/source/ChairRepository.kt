878f89bdbdbbdb0fe4904ce89f51659c0b17d643
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-10-19 20:34:18 +0800

    call remote data source in repository

--- app/src/main/java/com/example/reclaim/data/source/ChairRepository.kt
+++ app/src/main/java/com/example/reclaim/data/source/ChairRepository.kt
@@ -1,18 +1,34 @@
 package com.example.reclaim.data.source
 
+import com.example.reclaim.data.ChatRoom
+import com.example.reclaim.data.MessageType
 import com.google.firebase.firestore.CollectionReference
 
-interface ChairRepository {
+class ChairRepository(private val remoteDataSource: ChairRemoteDataSource) {
 
-    fun getAllRecordFromRoom(chatRoomKey: String): CollectionReference
 
-    fun updateSeenStatus(chatRoomID: String, documentID: String)
 
-    fun sendMessage(sendText: String, type: String, meetingID: String = "")
+    fun updateSeenStatus(chatRoomID: String, documentID: String){
+        remoteDataSource.updateSeenStatus(chatRoomID, documentID)
+    }
 
-    fun updateOnChatList(sendText: String, chatRoomKey: String, currentTime: String)
+    fun sendMessage(content: String, type: MessageType, meetingID: String = "", chatRoom: ChatRoom, chatRoomDocumentID: String){
+        remoteDataSource.sendMessage(content, type, meetingID, chatRoom, chatRoomDocumentID)
+    }
 
-    fun sendVideoCallMessage(meetingID: String)
+    fun updateOnChatList(content: String, chatRoomKey: String, currentTimeString: String){
+        remoteDataSource.updateOnChatList(content, chatRoomKey, currentTimeString)
+    }
 
-    fun turnOffJoinBtn()
+    fun sendVideoCallMessage(meetingID: String, chatRoom: ChatRoom, chatRoomDocumentID: String){
+        remoteDataSource.sendVideoCallMessage(meetingID, chatRoom, chatRoomDocumentID)
+    }
+
+    fun stopUserJoinMeeting(chatRoomDocumentID: String, meetingID: String){
+        remoteDataSource.stopUserJoinMeeting(chatRoomDocumentID, meetingID)
+    }
+
+    fun clearUnreadTimes(documentID: String){
+        remoteDataSource.clearUnreadTimes(documentID)
+    }
 }
