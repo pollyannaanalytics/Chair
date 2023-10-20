9149b955b1ece85072983668fb36c67d2ae3e96c
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-10-20 20:02:14 +0800

    add test case for chat room viewModel

--- app/src/test/java/com/example/reclaim/chatroom/ChatRoomViewModelTest.kt
+++ app/src/test/java/com/example/reclaim/chatroom/ChatRoomViewModelTest.kt
@@ -1,14 +1,34 @@
 package com.example.reclaim.chatroom
 
+import androidx.navigation.NavArgs
+import com.example.reclaim.data.source.ChairRepository
+import com.google.errorprone.annotations.DoNotMock
 import org.junit.Assert.*
 
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.webrtc.PeerConnection.Observer
 
 class ChatRoomViewModelTest {
+    @Mock
+    private lateinit var repository: ChairRepository
+
+    @Mock
+    private lateinit var navArgs: ChatRoomFragmentArgs
+
+    @Mock
+    private lateinit var viewModel: ChatRoomViewModel
+
+
+
 
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = ChatRoomViewModel(repository, navArgs = navArgs)
+
     }
 
     @Test
