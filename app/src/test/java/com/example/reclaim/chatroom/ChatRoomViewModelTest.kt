81f5a2e7b130ea0a7df58ea60d13d297aff589f8
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-10-21 11:18:45 +0800

    setup function in chatRoomviewmodel

--- app/src/test/java/com/example/reclaim/chatroom/ChatRoomViewModelTest.kt
+++ app/src/test/java/com/example/reclaim/chatroom/ChatRoomViewModelTest.kt
@@ -1,85 +1,40 @@
 package com.example.reclaim.chatroom
 
-import androidx.navigation.NavArgs
+import androidx.lifecycle.ViewModel
+import com.example.reclaim.data.ChatRoom
+import com.example.reclaim.data.source.ChairRemoteDataSource
 import com.example.reclaim.data.source.ChairRepository
-import com.google.errorprone.annotations.DoNotMock
-import org.junit.Assert.*
-
 import org.junit.Before
-import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
-import org.webrtc.PeerConnection.Observer
 
 class ChatRoomViewModelTest {
-    @Mock
-    private lateinit var repository: ChairRepository
 
-    @Mock
-    private lateinit var navArgs: ChatRoomFragmentArgs
-
-    @Mock
-    private lateinit var viewModel: ChatRoomViewModel
-
-
-
-
+    lateinit var chatRoomViewModel: ViewModel
+    lateinit var chairRepository: ChairRepository
+    lateinit var navArgs: ChatRoomFragmentArgs
+    lateinit var dataSource: ChairRemoteDataSource
     @Before
-    fun setUp() {
-        MockitoAnnotations.initMocks(this)
-        viewModel = ChatRoomViewModel(repository, navArgs = navArgs)
+    fun setUp(){
+        val fakeChatRoom = ChatRoom(
+            id = "1",
+            key = "chatRoomKey123",
+            selfId = "selfUserId",
+            otherId = "otherUserId",
+            selfName = "YourName",
+            otherName = "FriendName",
+            lastSentence = "Hello, how are you?",
+            sendById = "selfUserId",
+            selfImage = "selfImageUrl",
+            otherImage = "friendImageUrl",
+            sentTime = "2023-10-21 10:30:00",
+            unreadTimes = "3",
+            otherOnline = true
+        )
 
-    }
+        dataSource = ChairRemoteDataSource()
+        navArgs = ChatRoomFragmentArgs.Builder(fakeChatRoom).build()
+        chairRepository = ChairRepository(dataSource)
 
-    @Test
-    fun getFriendImage() {
-    }
 
-    @Test
-    fun getFriendName() {
-    }
-
-    @Test
-    fun getRecordWithFriend() {
-    }
-
-    @Test
-    fun getOnDestroyed() {
-    }
-
-    @Test
-    fun getJoinMeetingId() {
-    }
-
-    @Test
-    fun getRecordRegistraion() {
-    }
-
-    @Test
-    fun setRecordRegistraion() {
-    }
-
-    @Test
-    fun sendMessage() {
-    }
-
-    @Test
-    fun sendVideoCallMessage() {
-    }
-
-    @Test
-    fun joinMeeting() {
-    }
-
-    @Test
-    fun removeListener() {
-    }
-
-    @Test
-    fun stopUserJoinMeeting() {
-    }
-
-    @Test
-    fun onCleared() {
+        chatRoomViewModel = ChatRoomViewModel(chairRepository, navArgs)
     }
 }
---
+++ app/src/test/java/com/example/reclaim/data/source/ChairRemoteDataSourceTest.kt
@@ -0,0 +1,36 @@
+package com.example.reclaim.data.source
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ChairRemoteDataSourceTest {
+
+    @Test
+    fun getAllRecordFromRoom() {
+    }
+
+    @Test
+    fun clearUnreadTimes() {
+    }
+
+    @Test
+    fun updateSeenStatus() {
+    }
+
+    @Test
+    fun sendMessage() {
+    }
+
+    @Test
+    fun updateOnChatList() {
+    }
+
+    @Test
+    fun sendVideoCallMessage() {
+    }
+
+    @Test
+    fun stopUserJoinMeeting() {
+    }
+}
