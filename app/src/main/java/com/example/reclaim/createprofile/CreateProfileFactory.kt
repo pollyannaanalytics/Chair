04017710ef17feb7e9cde3e59f365e277d4f7f06
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-10-22 17:15:59 +0800

    set up factory for create profile viewModel

--- app/src/main/java/com/example/reclaim/createprofile/CreateProfileFactory.kt
+++ app/src/main/java/com/example/reclaim/createprofile/CreateProfileFactory.kt
@@ -1,4 +1,18 @@
 package com.example.reclaim.createprofile
 
-class CreateProfileFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.reclaim.chatroom.ChatRoomViewModel
+import com.example.reclaim.data.source.ChairRepository
+
+class CreateProfileFactory(
+    private val repository: ChairRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ChatRoomViewModel::class.java)) {
+            return CreateProfileViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException("Unknown viewModel class")
+    }
 }
