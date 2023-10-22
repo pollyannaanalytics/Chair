c21b8b391ea7e55938cb82c55305d51d120ad4bc
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-10-22 17:38:40 +0800

    move uploadImageToFireStorage function to data source

--- app/src/main/java/com/example/reclaim/data/source/ChairRemoteDataSource.kt
+++ app/src/main/java/com/example/reclaim/data/source/ChairRemoteDataSource.kt
@@ -1,12 +1,15 @@
 package com.example.reclaim.data.source
 
 import android.util.Log
+import androidx.core.net.toUri
 import com.example.reclaim.data.ChatRoom
 import com.example.reclaim.data.MessageType
 import com.example.reclaim.data.UserManager
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.storage.FirebaseStorage
+import java.util.UUID
 
 class ChairRemoteDataSource {
 
@@ -37,16 +40,16 @@ class ChairRemoteDataSource {
         const val MEETING_OVER_HINT = "通話已結束"
 
 
-
     }
 
-    fun getAllRecordFromRoom(chatRoomKey: String, callback: (Query, DocumentSnapshot) -> Unit){
+    fun getAllRecordFromRoom(chatRoomKey: String, callback: (Query, DocumentSnapshot) -> Unit) {
         db.collection(COLLECTION_CHAT_ROOM).whereEqualTo(ROOM_KEY, chatRoomKey)
             .get().addOnSuccessListener { snapshots ->
 
                 val room = snapshots.documents.first()
                 val recordRegistration = room.reference.collection(COLLECTION_CHAT_RECORD).orderBy(
-                    SENT_TIME, Query.Direction.ASCENDING)
+                    SENT_TIME, Query.Direction.ASCENDING
+                )
 
                 callback(recordRegistration, room)
             }
@@ -64,7 +67,7 @@ class ChairRemoteDataSource {
 
     }
 
-     fun updateSeenStatus(chatRoomID: String, documentID: String) {
+    fun updateSeenStatus(chatRoomID: String, documentID: String) {
         Log.i(TAG, "update seen status is trigger")
         val chatRoomIsSeenOrNot =
             db.collection(COLLECTION_CHAT_ROOM).document(chatRoomID).collection(
@@ -115,7 +118,7 @@ class ChairRemoteDataSource {
     }
 
 
-    fun updateOnChatList(content: String, chatRoomKey: String, currentTimeString: String){
+    fun updateOnChatList(content: String, chatRoomKey: String, currentTimeString: String) {
         var currentUnreadTime = 0
         val chatRoom = db.collection(COLLECTION_CHAT_ROOM).document(chatRoomKey)
         chatRoom.get().addOnSuccessListener {
@@ -139,17 +142,20 @@ class ChairRemoteDataSource {
     }
 
 
-
-
-
-    fun sendVideoCallMessage(meetingId: String, chatRoom: ChatRoom, chatRoomDocumentID: String, senderName: String, senderImageUri: String){
+    fun sendVideoCallMessage(
+        meetingId: String,
+        chatRoom: ChatRoom,
+        chatRoomDocumentID: String,
+        senderName: String,
+        senderImageUri: String
+    ) {
         val text = SEND_VIDEO_CALL_TEXT
         val type = MessageType.VIDEO_CALL
         sendMessage(text, type, meetingId, chatRoom, chatRoomDocumentID, senderName, senderImageUri)
     }
 
 
-    fun stopUserJoinMeeting(chatRoomDocumentID: String, meetingId: String){
+    fun stopUserJoinMeeting(chatRoomDocumentID: String, meetingId: String) {
 
         val chatRoom = db.collection(COLLECTION_CHAT_ROOM).document(chatRoomDocumentID)
         var chatRecordDocumentID = ""
@@ -158,7 +164,8 @@ class ChairRemoteDataSource {
 
                 chatRecordDocumentID = snapshot.documents.firstOrNull().let { it!!.id }
 
-                val chatRecordDocument = chatRoom.collection(COLLECTION_CHAT_RECORD).document(chatRecordDocumentID)
+                val chatRecordDocument =
+                    chatRoom.collection(COLLECTION_CHAT_RECORD).document(chatRecordDocumentID)
 
                 val dataUpdateToChatRecord = hashMapOf<String, Any>(
                     MEETING_OVER to true,
@@ -171,5 +178,29 @@ class ChairRemoteDataSource {
 
             }
     }
+
+
+    fun uploadImageToFireStorage(stringOfUri: String) {
+        var currentUri = ""
+        val reference = FirebaseStorage.getInstance().reference
+        val path = UUID.randomUUID().leastSignificantBits
+        val imageRef = reference.child("images/$path.jpg")
+
+        val uploadTask = imageRef.putFile(stringOfUri.toUri())
+
+        uploadTask.addOnSuccessListener { uri ->
+            imageRef.downloadUrl.addOnSuccessListener {
+                currentUri = it.toString()
+                UserManager.userImage = it.toString()
+                Log.i(TAG, "upload successfully, url is $it")
+            }
+
+
+        }
+        Log.i(TAG, "currentUri is $currentUri")
+
+
+    }
+
 
 }
